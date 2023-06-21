1335117e32a080e83bca43522755691e0db5e2ad
gcileno <gabriel_cileno@hotmail.com>
2023-06-21 15:30:47 -0300

    tentativa duelo 01

--- devduel/lib/player.dart
+++ devduel/lib/player.dart
@@ -3,10 +3,10 @@ import 'dart:math';
 class Player {
   final String nome;
   final int hp = 4000;
-  final List<Card> hand;
+  final List<Card> hand = [];
   final Deck deck;
 
-  Player(this.nome, this.hand, this.deck);
+  Player(this.nome, this.deck);
 
   void puxarCarta() {
     if (deck.baralho.length >= 2) {
@@ -21,15 +21,41 @@ class Player {
 }
 
 class Card {
+  final String image;
   final String nome;
   final int atk;
   final int def;
 
-  Card(this.nome, this.atk, this.def);
+  Card(this.image, this.nome, this.atk, this.def);
 }
 
 class Deck {
   final List<Card> baralho;
 
   Deck(this.baralho);
+}
+
+//
+//funções para criar baralhos e cards
+//
+Deck criarBaralho(List<dynamic> listaCartas) {
+  List<Card> cartas = [];
+
+  // Criar uma instância de Random
+  Random random = Random();
+
+  // Embaralhar as cartas de forma aleatória
+  List<dynamic> sorteio = List.from(listaCartas)..shuffle(random);
+
+  for (var i = 0; i < 10 && i < sorteio.length; i++) {
+    var cartaData = sorteio[i];
+    //criando a carta
+    Card carta = Card(cartaData['card_images'][0]['image_url_small'],
+        cartaData['name'], cartaData['atk'] ?? 0, cartaData['def'] ?? 0);
+
+    //inserindo no baralho
+    cartas.add(carta);
+  }
+
+  return Deck(cartas);
 }
